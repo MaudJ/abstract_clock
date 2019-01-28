f49f60928c412e74252fc8da869fc19ba6cfc7e4
Chongjiao Jiang <chongjiaojiang@dyn-160-39-132-76.dyn.columbia.edu>
2019-01-28 18:58:58 -0500

    Add simple code

--- sketch.js
+++ sketch.js
@@ -1,14 +1,26 @@
 function setup() {
-	createCanvas(800,600); // make an HTML canvas element width x height pixels
+	createCanvas(800,600); 
 }
 
 function draw() {
-	background(225);
-	textSize(32);
-	fill(180);
-	text(hour(), 10, 30);
-	fill(100);
-	text(minute(), 10, 60);
-	fill(0);
-	text(second(), 10, 90);
+	background(255);
+	
+	// circle
+	stroke(220,220, 220);
+	strokeWeight(4);
+	fill(255);
+	line(100,240,700,240);
+	
+	//hour
+	fill(int(hour()*255/23));
+	ellipse(100+int(hour()*600/23),240, 80, 80);
+	
+	//minute
+	fill(int(minute()*255/59));
+	ellipse(100+int(minute()*600/59),240, 80, 80);
+	
+	//second
+	fill(int(second()*255/59));
+	ellipse(100+int(second()*600/59),240, 80, 80);
 }
+
